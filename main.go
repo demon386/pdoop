434a2f040e5f425a5f342f027c5ad83ba3e3314b
M. Tong <demon386@gmail.com>
2017-01-03 19:59:39 +0800

    depend on HDFS

--- main.go
+++ main.go
@@ -3,72 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/demon386/hdfs"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
-	"strings"
 )
 
 var parallel = flag.Int("p", 10, "parallel")
 var mergeMode = flag.Bool("m", false, "merge as a single file")
 
-type HDFS struct {
-	hadoopHome string
-	hadoopCmd  string
-}
-
-func NewHDFS() HDFS {
-	hadoopHome := os.Getenv("HADOOP_HOME")
-	return HDFS{
-		hadoopHome: hadoopHome,
-		hadoopCmd:  path.Join(hadoopHome, "bin", "hadoop"),
-	}
-}
-
-func (s HDFS) Ls(inputPath string) []string {
-	log.Printf("ls: %s\n", inputPath)
-	output := s.Exec("fs", "-ls", inputPath)
-	outputSplit := strings.Split(output, "\n")
-	files := []string{}
-	for _, line := range outputSplit {
-		line = strings.TrimSpace(line)
-		if !strings.HasPrefix(line, "-") || line == "" {
-			continue
-		}
-		split := strings.Fields(line)
-		files = append(files, split[len(split)-1])
-	}
-	return files
-}
-
-func (s HDFS) Get(remote string, local string) {
-	s.Exec("fs", "-get", remote, local)
-}
-
-func (s HDFS) Gets(remoteFiles []string, local string) {
-	for _, f := range remoteFiles {
-		s.Get(f, local)
-	}
-}
-
-func (s HDFS) Exec(subCmd string, args ...string) string {
-	jobConf := []string{}
-	fullCmd := []string{
-		subCmd,
-	}
-	for _, args := range [][]string{jobConf, args} {
-		fullCmd = append(fullCmd, args...)
-	}
-	cmd := exec.Command(s.hadoopCmd, fullCmd...)
-	stdout, _ := cmd.CombinedOutput()
-	return string(stdout)
-}
-
-func downloadWithChan(hdfs HDFS, remoteFiles []string, localDir string, ch chan string) {
+func downloadWithChan(hdfs hdfs.HDFS, remoteFiles []string, localDir string, ch chan string) {
 	for _, f := range remoteFiles {
 		hdfs.Get(f, localDir)
 		localBase := filepath.Base(f)
@@ -87,7 +34,7 @@ func checkDir(path string) error {
 	return nil
 }
 
-func doMergeMode(hdfs HDFS, files []string, outputFile string) error {
+func doMergeMode(hdfs hdfs.HDFS, files []string, outputFile string) error {
 	var tempDir, _ = ioutil.TempDir("", "pdoop")
 	defer os.RemoveAll(tempDir)
 	log.Println("tempDir: ", tempDir)
@@ -152,7 +99,7 @@ func mergeFiles(resultsCh <-chan string, count int, output string) error {
 }
 
 // put files into a directory
-func doFilesMode(hdfs HDFS, files []string, outputDir string) error {
+func doFilesMode(hdfs hdfs.HDFS, files []string, outputDir string) error {
 	filesCh := make(chan string)
 	resultsCh := make(chan struct{})
 	defer close(resultsCh)
@@ -192,8 +139,12 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	} else {
+		if _, err := os.Stat(output); err == nil {
+			log.Fatalf("target file %s already exists\n", output)
+		}
 	}
-	hdfs := NewHDFS()
+	hdfs := hdfs.NewHDFS()
 	files := hdfs.Ls(input)
 	if *mergeMode {
 		doMergeMode(hdfs, files, output)
